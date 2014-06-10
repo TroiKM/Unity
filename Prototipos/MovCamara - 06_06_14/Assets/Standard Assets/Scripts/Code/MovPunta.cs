47c97bffe63d6e7ac3f09a4887af500825262f58
TroiKM <karentroiano2009@gmail.com>
2014-06-10 11:15:40 -0430

    Oficial first commit

---
+++ Prototipos/MovCamara - 06_06_14/Assets/Standard Assets/Scripts/Code/MovPunta.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovPunta : MonoBehaviour {
+
+	public float rotaCam;
+
+	// Use this for initialization
+	void Start () {
+	
+				rotaCam = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		// l-> girto der, k -> giro izq.
+		if (Input.GetKey("k"))
+			this.transform.Rotate(0, 0, -rotaCam * Time.deltaTime);
+		if (Input.GetKey ("l"))
+			this.transform.Rotate(0, 0, rotaCam * Time.deltaTime);
+
+	}
+}
