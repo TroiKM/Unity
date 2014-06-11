dacfa95735ca5e7255d5a358c5598f1d06b3ba58
TroiKM <karentroiano2009@gmail.com>
2014-06-11 12:38:22 -0430

    Miércoles 11/06/2014
    - Cámara lista con cursor. (Aún por revisar)
    -  Se inicia a tratar la parte touch.

---
+++ MovCamara/Assets/Standard Assets/Scripts/Code/TouchInput.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchInput : MonoBehaviour {
+
+	void Update () {
+	
+	}
+}
---
+++ Prototipos/MovCamara - 11_06_14/Assets/Standard Assets/Scripts/Code/CollisionCamera.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionCamera : MonoBehaviour
+{
+	void OnTriggerStay(){
+		Debug.Log("Hit the wall");
+	}
+}
