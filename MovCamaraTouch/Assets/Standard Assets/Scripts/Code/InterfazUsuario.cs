a7b70c157d28f2f7fc3fadebf345f8f43ecccee9
TroiKM <karentroiano2009@gmail.com>
2014-06-16 09:49:42 -0430

    Lunes 16/06/2014

--- MovCamaraTouch/Assets/Standard Assets/Scripts/Code/InterfazUsuario.cs
+++ MovCamaraTouch/Assets/Standard Assets/Scripts/Code/InterfazUsuario.cs
@@ -1,20 +1,39 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class InterfazUsuario : MonoBehaviour {
 
-	// Update is called once per frame
-	void Update () {
+	public GUISkin myGui = null;
+	public GUISkin artro = null;
+	public Texture2D check = null;
+	public Texture2D edit = null;
+	public Texture2D question = null;
+
+	void Update () 
+	{
 	
 	}
 
 	void OnGUI()
 	{
+		if (myGui)
+		{
+			GUI.skin = myGui;
+		}
 
 		GUI.Box(new Rect(Screen.width/2 - 150, Screen.height/2, 300, 300), "");
+		GUI.Box(new Rect(Screen.width/2 - 150, Screen.height/2, 300, 40), "");
 
-		GUI.Button(new Rect(Screen.width/2 - 200, Screen.height/2,85,25), "BOTON1");
-	//	GUI.Button(new Rect(,85,25), "BOTON2");
-	//	GUI.Button(new Rect(,85,25), "BOTON3");
+		GUI.Button(new Rect(Screen.width/2 - 145, Screen.height/2 + 5,95,30), edit);
+		GUI.Button(new Rect(Screen.width/2 - 47, Screen.height/2 + 5,95,30), question);
+		GUI.Button (new Rect (Screen.width / 2 + 50, Screen.height / 2 + 5, 95, 30), check);
+
+		if (artro)
+		{
+			GUI.skin = artro;
+		}
+
+		GUI.Box(new Rect(Screen.width/2 - 145, Screen.height/2 + 150, 42, 42), "");
+		GUI.Button(new Rect(Screen.width/2 + 103, Screen.height/2 + 150, 42, 42), "");
 	}
 }
